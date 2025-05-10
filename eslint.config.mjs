21404cd8cd700b1c056db90865554a63d81e0d73
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-05-10 12:06:55 -0300

    build(eslint): add eslint perfectionist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from 'eslint-plugin-perfectionist'
+import { defineConfig } from "eslint/config";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,26 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+const eslintConfig = defineConfig(
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  perfectionist.configs['recommended-alphabetical'],
+  {
+    rules: {
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          groups: [
+            ['value-builtin', 'value-external', 'type-builtin', 'type-external'],
+            ['type-internal', 'value-internal'],
+            ['type-parent', 'type-sibling', 'type-index', 'value-parent', 'value-sibling', 'value-index'],
+            'side-effect-style',
+            'unknown',
+          ],
+          internalPattern: ['@/.*'],
+        },
+      ],
+    },
+  },
+);
 
 export default eslintConfig;
