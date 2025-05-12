d77bf4ea52636c2195299ad6a14e6100081829b1
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-05-11 21:35:42 -0300

    build(api): set up the prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { prisma } from '@/lib/prisma';
+
+export default async function Home() {
+  const users = await prisma.usuarios.findMany();
+  console.log(users);
+
   return <p>Hello, World!!</p>;
 }
