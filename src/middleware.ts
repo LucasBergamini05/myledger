bd5e2b63ae55685ed8d6cfa3a3a7546abb9748a7
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-05-18 15:15:37 -0300

    feat(route): add middleware to set initial page

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { getCurrentUser } from '@/utils/auth';
+
+export const middleware = async (request: NextRequest) => {
+  if (request.nextUrl.pathname !== '/') return NextResponse.next();
+
+  const isUserLogged = !!(await getCurrentUser());
+
+  // Changes base URL to /app when user is logged in
+  if (isUserLogged) return NextResponse.rewrite(new URL('/app', request.url));
+
+  // Changes base URL to /login when user is not logged in
+  return NextResponse.rewrite(new URL('/login', request.url));
+};
