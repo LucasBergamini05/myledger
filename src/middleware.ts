be69622e5af806780cb2a77e083549fbf4fceed9
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-05-19 19:13:27 -0300

    fix(route): fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,13 +13,15 @@ export const middleware = async (request: NextRequest) => {
       // If user is not logged in, redirect to /login
       return NextResponse.redirect(new URL('/login', request.url));
 
+    case '/home':
+      // If user is not logged in, redirect to /login
+      if (!isUserLogged) return NextResponse.redirect(new URL('/login', request.url));
+      break;
     case '/login':
+
     case '/register':
       // If user is logged in, redirect to /home
       if (isUserLogged) return NextResponse.redirect(new URL('/home', request.url));
-
-    default:
-      // If user is not logged in, redirect to /login
-      if (!isUserLogged) return NextResponse.redirect(new URL('/login', request.url));
+      break;
   }
 };
