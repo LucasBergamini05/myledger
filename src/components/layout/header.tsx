f427034b7f1e5687e341687f5b31d8a21f480522
Lucas Bergamini <lucas.bergamini05@outlook.com>
2025-05-20 12:23:01 -0300

    style(layout): increase header padding x,

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,7 @@
-import Link from 'next/link';
-
 import { Profile } from './profile';
 
 export const Header = () => (
-  <header className="w-full h-16 p-2 flex items-center justify-between bg-zinc-900 ">
+  <header className="w-full h-16 py-2 px-8 flex items-center justify-between bg-zinc-900 ">
     <h1 className="text-xl">MyLedger</h1>
 
     <Profile className="flex-1" />
